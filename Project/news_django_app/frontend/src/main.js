57439e8a10a7d90ab686e3f23dd5ea5cb2ceccc6
FDMan12 <vdmmsh2003@mail.ru>
2024-05-08 15:15:01 +0300

    Fixed router old paths

--- Project/news_django_app/frontend/src/main.js
+++ Project/news_django_app/frontend/src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/router.js'; // Импортируем файл маршрутизации
+    import { createApp } from 'vue'
+    import App from './App.vue'
+    import router from './router/router.js'; // Импортируем файл маршрутизации
 
-createApp(App).use(router).mount('#app')
+    const app = createApp(App);
+    app.use(router)
+    app.mount('#app');
 
--- Project/news_django_app/frontend/src/router/router.js
+++ Project/news_django_app/frontend/src/router/router.js
@@ -1,14 +1,11 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router'
-import Main from './../views/main.vue'
-import Signup from './../views/signup.vue'
-import Login from './../views/login.vue'
-import ChooseTags from './../views/choose_tags.vue'
-import News from './../views/news.vue'
-import Magazines from './../views/magazines.vue'
-import Account from './../views/account.vue'
-
-Vue.use(VueRouter)
+import { createRouter, createWebHistory } from 'vue-router';
+import Main from './../views/main.vue';
+import Signup from './../views/signup.vue';
+import Login from './../views/login.vue';
+import ChooseTags from './../views/choose_tags.vue';
+import News from './../views/news.vue';
+import Magazines from './../views/magazines.vue';
+import Account from './../views/account.vue';
 
 const routes = [
   { path: '/', component: Main },
@@ -18,11 +15,11 @@ const routes = [
   { path: '/news', component: News },
   { path: '/magazines', component: Magazines },
   { path: '/account', component: Account }
-]
+];
 
-const router = new VueRouter({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory(),
   routes
-})
+});
 
 export default router;
