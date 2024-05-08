81bbebe82855da7fce842371e2b0987e6febfb75
FDMan12 <vdmmsh2003@mail.ru>
2024-05-08 14:43:20 +0300

    Project build

--- Project/news_django_app/frontend/src/views/main.vue
+++ Project/news_django_app/frontend/src/views/main.vue
@@ -1,58 +1,450 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="page">
+    <div class="search-bar">
+      <input type="text" placeholder="Поиск..." class="search-input">
+    </div>
+
+    <div class="news-for-you">
+      <span style="font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; color: #3d3d3d;">
+        Новости для вас
+        </span>
+    </div>
+
+    <div class="my-magazines-button">
+      <span style="font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; color: #3d3d3d;">
+        Мои журналы
+        </span>
+    </div>
+
+    <div class="todays">
+      <span style="font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; color: #3d3d3d;">
+        Сегодняшнее
+        </span>
+    </div>
+
+    <div class="mynews">
+      <span style="font-family: Arial, sans-serif; font-size: 14px; font-weight: bold; color: #3d3d3d;">
+          Мои новости
+        </span>
+    </div>
+
+    <div class="chosentag">
+      <span style="font-family: Arial, sans-serif; font-size: 14px; font-weight: bold; color: #3d3d3d;">
+          Избранные теги
+        </span>
+    </div>
+
+    <div class="chosentags">
+      <span style="font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; color: black;">
+          Спорт<br>Музыка<br>Кино<br>Наука
+        </span>
+    </div>
+
+    <div class="local-new">
+      <span style="font-family: Arial, sans-serif; font-size: 14px; font-weight: bold; color: #3d3d3d;">
+          Местные новости
+        </span>
+    </div>
+
+    <div class="local-news">
+      <span style="font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; color: black;">
+          Москва<br>Липецк<br>Воронеж<br>Пермь
+        </span>
+    </div>
+
+    <div class="my-magazine">
+      <span style="font-family: Arial, sans-serif; font-size: 14px; font-weight: bold; color: #3d3d3d;">
+          Мои журналы
+        </span>
+    </div>
+
+    <div class="my-magazines">
+      <span style="font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; color: black;">
+          New York Times<br>Биографии<br>Esquire<br>Elle
+        </span>
+    </div>
+
+    <div class="left-menu">
+      <div class="news-link" @click="goToNewsPage" style="margin-left: 250px;">
+        <span style="font-family: Arial, sans-serif; font-size: 20px; font-weight: bold; color: white;">
+          Мои<br>Новости
+        </span>
+      </div>
+    </div>
+
+    <div class="top-block">
+      <div class="account-info" @click="goToAccountPage">
+        <div class="circle"></div>
+        <div class="account-text">
+          Мой<br>Аккаунт
+        </div>
+      </div>
+    </div>
+
+    <div class="news1" @click="goToAccountPage">
+        <div class="square1"></div>
+        <div class="text1">
+          New York Times<br><br>Лучшие кофейни в Нью-Йорке по версии Шакиры
+        </div>
+      </div>
+      <div class="news2" @click="goToAccountPage">
+        <div class="square2"></div>
+        <div class="text2">
+          New York Times<br><br>Лучшие кофейни в Нью-Йорке по версии Шакиры
+        </div>
+      </div>
+      <div class="news3" @click="goToAccountPage">
+        <div class="square3"></div>
+        <div class="text3">
+          New York Times<br><br>Лучшие кофейни в Нью-Йорке по версии Шакиры
+        </div>
+      </div>
+
+    <div class="right-block"></div>
+    <div class="bottom-block">
+      <div class="footer-links">
+        <div class="footer-link" @click="goToAboutPage">О нас</div>
+        <div class="footer-link" @click="goToHelpPage">Помощь</div>
+        <div class="footer-link" @click="goToContactsPage">Контакты</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'App',
+  methods: {
+    goToNewsPage() {
+      // Реализуйте переход на страницу новостей
+    },
+    goToAccountPage() {
+      // Реализуйте переход на страницу аккаунта
+    },
+    goToAboutPage() {
+      // Реализуйте переход на страницу "О нас"
+    },
+    goToHelpPage() {
+      // Реализуйте переход на страницу "Помощь"
+    },
+    goToContactsPage() {
+      // Реализуйте переход на страницу "Контакты"
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.page {
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  height: 200vh;
+  position: relative;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.top-block {
+  height: 157px;
+  background-color: #628348;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.account-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
 }
-a {
-  color: #42b983;
+
+.circle {
+  position: absolute;
+  top: 10px;
+  left: 1800px;
+  width: 70px;
+  height: 70px;
+  background-color: #777777;
+  border-radius: 50%;
+  margin-right: 20px;
+  cursor: pointer;
 }
+
+.account-text {
+  position: absolute;
+  top: 90px;
+  left: 1825px;
+  color: white;
+  font-size: 16px;
+  font-family: Arial, sans-serif;
+  cursor: pointer;
+  text-align: center;
+  margin-left: -20px;
+}
+
+.square1 {
+  position: absolute;
+  top: 200px;
+  left: 300px;
+  width: 400px;
+  height: 400px;
+  background-color: #628348;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.text1 {
+  position: absolute;
+  top: 620px;
+  left: 300px;
+  width: 400px;
+  height: 400px;
+  color: Black;
+  font-size: 20px;
+  font-family: Arial, sans-serif;
+  cursor: pointer;
+  text-align: left;
+  margin-left: 0px;
+}
+
+.square2 {
+  position: absolute;
+  top: 200px;
+  left: 875px;
+  width: 400px;
+  height: 400px;
+  background-color: #628348;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.text2 {
+  position: absolute;
+  top: 620px;
+  left: 875px;
+  width: 400px;
+  height: 400px;
+  color: Black;
+  font-size: 20px;
+  font-family: Arial, sans-serif;
+  cursor: pointer;
+  text-align: left;
+  margin-left: 0px;
+}
+
+.square3 {
+  position: absolute;
+  top: 200px;
+  left: 1450px;
+  width: 400px;
+  height: 400px;
+  background-color: #628348;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.text3 {
+  position: absolute;
+  top: 620px;
+  left: 1450px;
+  width: 400px;
+  height: 400px;
+  color: Black;
+  font-size: 20px;
+  font-family: Arial, sans-serif;
+  cursor: pointer;
+  text-align: left;
+  margin-left: 0px;
+}
+
+.left-menu {
+  width: 250px;
+  min-height: 0;
+  height: calc(600vh - 157px);
+  background-color: #AAA58C;
+  position: relative;
+  z-index: 2;
+}
+
+.news-link {
+  padding: 20px;
+  cursor: pointer;
+}
+
+.right-block {
+  flex-grow: 1;
+  background-color: #ffffff;
+}
+
+.bottom-block {
+  height: 250px;
+  background-color: #252424;
+  width: 100%;
+}
+
+.footer-links {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 60%;
+  padding: 40px 381px;
+}
+
+.footer-link {
+  color: white;
+  font-family: Arial, sans-serif;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.search-bar {
+  position: absolute;
+  top: 100px;
+  left: 20px;
+  width: 200px;
+  height: 25px;
+  background-color: white;
+  border-radius: 5px;
+  padding: 5px 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  z-index: 999;
+}
+
+.news-for-you {
+  position: absolute;
+  top: 100px;
+  left: 500px;
+  width: 200px;
+  height: 25px;
+  background-color: white;
+  border-radius: 7px;
+  padding: 7px 5px;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+}
+
+.my-magazines-button {
+  position: absolute;
+  top: 98px;
+  left: 925px;
+  width: 200px;
+  height: 25px;
+  background-color: white;
+  border-radius: 5px;
+  padding: 7px 5px;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+}
+
+.todays {
+  position: absolute;
+  top: 98px;
+  left: 1350px;
+  width: 200px;
+  height: 25px;
+  background-color: white;
+  border-radius: 5px;
+  padding: 7px 5px;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+}
+
+.search-input {
+  width: 100%; /* Ширина поля ввода равна ширине родительского элемента */
+  height: 100%; /* Высота поля ввода равна высоте родительского элемента */
+  border: none;
+  outline: none;
+  font-size: 16px;
+}
+
+.mynews{
+  position: absolute;
+  top: 170px;
+  left: 10px;
+  width: 200px;
+  height: 25px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  z-index: 999;
+}
+
+.chosentag{
+  position: absolute;
+  top: 300px;
+  left: 10px;
+  width: 200px;
+  height: 25px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  z-index: 999;
+}
+
+.chosentags{
+  position: absolute;
+  top: 330px;
+  left: 60px;
+  width: 200px;
+  height: 25px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  z-index: 999;
+}
+
+.local-new{
+  position: absolute;
+  top: 450px;
+  left: 10px;
+  width: 200px;
+  height: 25px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  z-index: 999;
+}
+
+.local-news{
+  position: absolute;
+  top: 480px;
+  left: 60px;
+  width: 200px;
+  height: 25px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  z-index: 999;
+}
+
+.my-magazine{
+  position: absolute;
+  top: 600px;
+  left: 10px;
+  width: 200px;
+  height: 25px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  z-index: 999;
+}
+
+.my-magazines{
+  position: absolute;
+  top: 630px;
+  left: 60px;
+  width: 200px;
+  height: 25px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  z-index: 999;
+}
+
 </style>
+
